3ec2fb46ac4badf777b5641c4b0b82ffad863bda
ItsAMeRishabh <rishabh.bansal1598@gmail.com>
2022-06-03 13:06:17 +0530

    Movement Partially Done
    -Movement Partially Executed
    -URP implementation
    -Post Processing Implementation

---
+++ CyberSpin/Assets/Scripts/RotationMovement.cs
@@ -0,0 +1,159 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationMovement : MonoBehaviour
+{
+    //Rotation Variables
+    [SerializeField] private float angularTorqueVal;
+    [SerializeField] private float maxAngularVelocity;
+    private float currAngularVelocity;
+
+    //Acceleration Variables
+    public float accelRatePerSec;
+    public float maxSpeed;
+    public float ballVelocity;
+
+    //Deceleration Variables
+    public float decelRatePerSec;
+
+    //Deceleration Rotation Variables
+    public float decelRotRatePerSec;
+
+    private Rigidbody2D rb;
+
+    public bool inputGiven;
+
+    public float moveInput;
+
+    //Time Variables
+    public float movetimeMaxToZero;
+    public float movetimeZeroToMax;
+    public float rottimeMaxToZero;
+
+    //Up Boost Variables
+    [SerializeField] private float upBoostSpeed;
+    private bool upBoostOn;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        inputGiven = false;
+        upBoostOn = false;
+
+        accelRatePerSec = maxSpeed / movetimeZeroToMax;
+        decelRotRatePerSec = -maxAngularVelocity / rottimeMaxToZero;
+
+        //Debugging Decel
+        decelRatePerSec = -maxSpeed / movetimeMaxToZero;
+
+        ballVelocity = 0f;
+    }
+
+    private void Update()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+
+        currAngularVelocity = rb.angularVelocity;
+
+        rb.velocity = Vector2.right * ballVelocity;
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.A) && !upBoostOn)
+        {
+
+            MovementCode(1);
+
+        }
+        else if (Input.GetKey(KeyCode.D) && !upBoostOn)
+        {
+
+            MovementCode(-1);
+
+        }
+        else
+        {
+            inputGiven = false;
+        }
+
+        UpBoostMovement();
+
+    }
+
+    void MovementCode(float mag)
+    {
+        //Rotation Code
+        if (currAngularVelocity < maxAngularVelocity && currAngularVelocity > -maxAngularVelocity)
+        {
+            rb.AddTorque(angularTorqueVal * mag);
+        }
+
+        //Move Script
+        ballVelocity += -accelRatePerSec * Time.deltaTime * mag;
+        ballVelocity = Mathf.Clamp(ballVelocity, -maxSpeed, maxSpeed);
+
+        inputGiven = true;
+    }
+
+    void UpBoostMovement()
+    {
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = Vector2.zero;
+            rb.velocity += Vector2.up * upBoostSpeed;
+            upBoostOn = true;
+        }
+        else
+        {
+            upBoostOn = false;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if(!inputGiven)
+        {
+            if (rb.angularVelocity > 0)
+            {
+                currAngularVelocity += decelRotRatePerSec * Time.deltaTime;
+                currAngularVelocity = Mathf.Max(currAngularVelocity, 0f);
+                rb.angularVelocity = currAngularVelocity;
+            }
+
+            if (rb.angularVelocity < 0)
+            {
+                currAngularVelocity += -decelRotRatePerSec * Time.deltaTime;
+                currAngularVelocity = Mathf.Min(currAngularVelocity, 0f);
+                rb.angularVelocity = currAngularVelocity;
+            }
+
+            // Decel incomplete
+            /*if (moveInput > 0)
+            {
+                Debug.Log("Positive");
+
+                ballVelocity += decelRatePerSec * Time.deltaTime;
+                ballVelocity = Mathf.Min(ballVelocity, 0f);
+                rb.velocity = transform.right * ballVelocity;
+                if (rb.velocity.x < 0f)
+                {
+                    Debug.Log("HEY !");
+                    rb.velocity = Vector2.zero;
+                }
+
+            }
+
+            if (moveInput < 0)
+            {
+                Debug.Log("Negative");
+            
+                ballVelocity += -decelRatePerSec * Time.deltaTime;
+                ballVelocity = Mathf.Max(ballVelocity, 0f);
+                rb.velocity = transform.right * ballVelocity;
+            }*/
+        }
+
+    }
+}
