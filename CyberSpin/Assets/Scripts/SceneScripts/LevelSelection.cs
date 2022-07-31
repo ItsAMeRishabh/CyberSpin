a32649a3f88995734a1bdd819553e2a2718377e2
Vicky3252 <83200625+Vicky3252@users.noreply.github.com>
2022-08-01 00:00:51 +0530

    Audio Updates

--- CyberSpin/Assets/Scripts/SceneScripts/LevelSelection.cs
+++ CyberSpin/Assets/Scripts/SceneScripts/LevelSelection.cs
@@ -7,20 +7,37 @@ using UnityEngine.EventSystems;
 
 public class LevelSelection : MonoBehaviour
 {
+    public Animator transition;
+
+    public Animator musicAnim;
+
+    public float transitionTime = 1f;
+
     private string buttonName;
     private char levelNumber;
     private int selectedLevel;
 
-    public void LoadLevel()
+    public void ChangeScene(int sceneIndex)
     {
+        StartCoroutine(LoadLevel(sceneIndex));
+    }
+
+    IEnumerator LoadLevel(int sceneIndex)
+    {
+        transition.SetTrigger("Start");
+
+        musicAnim.SetTrigger("FadeOut");
+
         buttonName = EventSystem.current.currentSelectedGameObject.name;  //Check Name of level pressed
         levelNumber = buttonName[6];                                      //Checking Number from level string
         selectedLevel = levelNumber - '0';                                //String -> int
 
         LevelManager.currentLevel = selectedLevel;                        //Setting to Level to load
-        LevelManager.hasToUpdate = true;                                  //Has to (?) update variables on level load
+        LevelManager.hasToUpdate = true;    
 
-        SceneManager.LoadScene("Level 1");                                //Loading Levels Scene
+        yield return new WaitForSeconds(transitionTime);                              //Has to (?) update variables on level load
+
+        SceneManager.LoadScene(sceneIndex);                                //Loading Levels Scene
     }
 
 }
