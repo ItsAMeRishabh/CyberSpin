e65cf03c1affe41ad9eee97deabca81424c9ef4e
Rishabh Gupta <93116645+ItsAMeRishabh@users.noreply.github.com>
2023-01-04 22:15:09 +0530

    Level 2 0.2 Update

--- CyberSpin/Assets/Scripts/Level Script/Level2 Script/Level2CamSwitch.cs
+++ CyberSpin/Assets/Scripts/Level Script/Level2 Script/Level2CamSwitch.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Level2CamSwitch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //instance
+    public static Level2CamSwitch instanceCamSwitch;
+
+    //VirtualCams
+    [SerializeField] private CinemachineVirtualCamera zoomOutCam;
+    [SerializeField] private CinemachineVirtualCamera playerCam;
+
+    //Variables
+    private float timer;
+    private bool isSwitching;
+
+    //GameObjects
+    [SerializeField] private GameObject player;
+    public GameObject victoryScreen;
+
+    private void Awake()
     {
-        
+        instanceCamSwitch = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        SwitchCamera();
+    }
+
+    //Cinemachine Camera Switch Script
+    private void SwitchCamera()
+    {
+        if (isSwitching)
+        {
+            //While Timer is positive
+            if (timer > 0)
+            {
+                zoomOutCam.Priority = 1;                                //Switch to Zoomed Out Camera
+                playerCam.Priority = 0;                                 //Switch off from Player Camera
+                timer -= Time.deltaTime;                                //Deducting time for every Time.deltaTime
+            }
+
+            else
+            {
+                LevelManager.currentLevel++;                            //Switch to next level
+                zoomOutCam.Priority = 0;                                //Switch off from Zoomed out cam
+                playerCam.Priority = 1;                                 //Switch to Player Cam
+                LevelManager.instanceLevelManager.toNextPos();          //Update Character Pos to Next Level
+                isSwitching = false;
+
+                ButtonScript.instanceButtonScript.isActivated = false;
+                ButtonScript.instanceButtonScript.canActivate = false;
+
+                player.GetComponent<CharacterController>().enabled = true;   //Turn Player Scripts on
+                player.GetComponent<CharacterJump>().enabled = true;
+
+                timer = 0;
+            }
+        }
+
     }
 }
